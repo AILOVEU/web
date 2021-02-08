bf510dc4f1dcc705e535d3cb08bf781010032f32
王欢26 <huan26.wang@zhangmen.com>
2021-02-08 15:31:19 +0800

    feat: html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname+'/dist'),
     filename: '[name].build.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico'
+    })
+  ]
 }
 module.exports = config;
