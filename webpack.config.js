f4da4456d32ae87328d4a396b4bedddfa74be974
王欢26 <huan26.wang@zhangmen.com>
2021-02-08 15:39:04 +0800

    feat: devServer

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,17 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico'
+      filename: 'index.html', // 生成的文件名
+      template: 'public/index.html', // 模板html
+      favicon: 'public/favicon.ico' // 图标
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname,'dist'), // html所在路径
+    compress: true, // 是否压缩
+    port: 3000, // 端口
+    hot: true, // 热部署
+    open: true // 打包完成后自动打开网页
+  }
 }
 module.exports = config;
